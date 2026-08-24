536c2255314144d32b3d1e5d8ad4f95f1c670cb3
rigsue <63635919+rigsue@users.noreply.github.com>
2026-08-24 11:31:19 +0800

    Add onboarding routes for HR, IT, Finance, and more

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Certifications from './pages/Certifications';
 import ManageAccount from './pages/ManageAccount';
+import HR from './pages/onboard/hr';
+import IT from './pages/onboard/it';
+import Finance from './pages/onboard/finance';
+import Marketing from './pages/onboard/marketing';
+import PreSales from './pages/onboard/presales';
+import CustomerSuccess from './pages/onboard/cust_success';
+import Sales from './pages/onboard/sales';
+// import PPM from './pages/onboard/ppm';
+// import OOC from './pages/onboard/ooc';
 
 const App = () => {
   return (
@@ -22,6 +31,17 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/certifications" element={<Certifications />} />
           <Route path="/manageaccount" element={<ManageAccount />} />
+                  {/* onboarding */}
+        <Route path="/hr" element={<HR />} />
+        <Route path="/it" element={<IT />} />
+        <Route path="/finance" element={<Finance />} />
+        <Route path="/marketing" element={<Marketing />} />
+        <Route path="/presales" element={<PreSales />} />
+        <Route path="/cust_success" element={<CustomerSuccess />} />
+        <Route path="/sales" element={<Sales />} />
+        {/* <Route path="/ppm" element={<PPM />} />
+        <Route path="/ooc" element={<OOC />} /> */}
+          
         </Routes>
       </BrowserRouter>
     </ProfileProvider>
